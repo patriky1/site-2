113bff2ec0d620b26fd6e8fcd9c4e389a9517841
patriky1 <patrikybrito@gmail.com>
2022-06-16 22:25:42 -0300

    up

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import {GridPerfil} from "./componentes/grid/grid1";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+
+
+
+function App() {
+  return (
+<>
+<section id="home" className="background">
+<GridPerfil/>
+</section>
+
+
+
+</>
+  );
+}
+
+export default App;
